ec331e0b3c9aa61981dac70f3d62f27717e31131
Hans van Wijk <hans.v.wijk83@gmail.com>
2024-05-15 22:11:24 +0200

    Serve JSON on a specific route

--- myApp.js
+++ myApp.js
@@ -2,13 +2,19 @@ let express = require("express");
 let app = express();
 const PORT = process.env.PORT || 3000;
 const IndexView = __dirname + "/views/index.html";
-const publicFolder = __dirname + "/public";
+// const publicFolder = __dirname + "/public";
 
 app.get("/", (req, res) => {
   res.sendFile(IndexView);
 });
 
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json",
+  });
+});
+
 // app.use("public", express.static(publicFolder));
-app.use("/public", express.static(publicFolder));
+app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
