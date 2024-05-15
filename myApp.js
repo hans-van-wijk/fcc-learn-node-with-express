13118807cd442cdd71851315d76fdeb2244d7f90
Hans van Wijk <hans.v.wijk83@gmail.com>
2024-05-15 21:48:09 +0200

    -

--- myApp.js
+++ myApp.js
@@ -1,39 +1,9 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
